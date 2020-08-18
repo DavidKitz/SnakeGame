911c8420199746936256ed1fe9b2d3fb3399e630
David Kitz <david.kitzwork@gmail.com>
2020-08-18 18:37:45 +0200

    updated the game logic

--- snake.js
+++ snake.js
@@ -4,39 +4,55 @@ const ctx=canv.getContext("2d");
 let imgName= new Image();
 let snake= [];
 canv.style.border="2px solid black";
-let x=275;
-let y=275;
-let box;
-ctx.rect(x,y,25,25);
-ctx.stroke();
-ctx.clearRect(x,y,25,25);
+
+let box={x:275, y:275}
 
 let direct;
 document.addEventListener("keydown",move);
-
+setInterval(drawIt,30);
 
 function move(event) {
-if (event.keyCode===38) {
+if (event.keyCode===40) {
+   
     direct="UP";
-    ctx.clearRect(x,y,25,25);
-    y+=25;
+    box["y"]+=25;
+    drawIt();
+
 }
- else if (event.keyCode===40) {
+ else if (event.keyCode===38) {
+   
     direct="DOWN";
-    y-=25;
+    box["y"]-=25;
+    drawIt();
+    
  }
  else if (event.keyCode===37) {
+   
     direct="LEFT";
-    x-=25;
+    box["x"]-=25;
+    drawIt();
+    
 }
  else if (event.keyCode===39) {
+   
     direct="RIGHT";
-    x+=25;
-    
+    box["x"]+=25;
+    drawIt();
     
  }
 }
+function clear (c) {
+   c.clearRect(0,0,600,600);
+}
 function drawIt() {
+   if (box["x"]>=600) {
 
+      return;
+   }
+   if (box["y"]>=600) {
+      return;
+   }
+   clear(ctx);
+   ctx.fillRect(box["x"], box["y"], '25', '25');
 
 }

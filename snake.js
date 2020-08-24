6a88af0822441ef1f1c2aa1c652d47d63f12d941
unknown <david.kitzwork@gmail.com>
2020-08-24 15:13:56 +0200

    added score and highscore

--- snake.js
+++ snake.js
@@ -1,16 +1,23 @@
 //box geht 22mal in eine länge(550/25) 
-// Fix movement+ adding to snake not working properly
+// Try to increase hitbox of food -- add point increment when snake add
+// +add the point to the canvas
 const canv=document.getElementById("canvas");
 const ctx=canv.getContext("2d");
+const score=document.getElementById("score");
+const highscore=document.getElementById("highscore");
 let imgName= new Image();
 let box=25;
 let food={x:Math.floor(Math.random()*22)*box, 
 y:Math.floor(Math.random()*20)*box};
 let snake= [{x:11*box, y:11*box}];
 let snakeHead=snake[0];
-let points;
+let points=0;
+let maxpoints=0;
 let direct;
+score.innerHTML= points;
+highscore.innerHTML= "Highscore: "+maxpoints;
 canv.style.border="2px solid black";
+checkHighscore();
 
 
 document.addEventListener("keydown",move);
@@ -40,13 +47,16 @@ function clear (c) {
 function drawIt() {
    for (let i=1;i<snake.length;i++) {
       if (snake[i].x===snake[0].x && snake[i].y===snake[0].y) {
+         checkHighscore();
          return;
       }
    }
    if (snake[0]["x"]>=600||snake[0]["x"]<=-25 ) {
+      checkHighscore();
       return;
    }
    if (snake[0]["y"]>=600||snake[0]["y"]<=-25) {
+      checkHighscore();
       return;
    }
    if (snake[0]["x"]===food["x"] && snake[0]["y"]===food["y"]){
@@ -78,24 +88,40 @@ function drawFood() {
    ctx.arc(food["x"], food['y'],12.5,0,Math.PI * 3);
    ctx.stroke();
 }
+function checkHighscore() {
+   if (points>maxpoints) {
+      maxpoints=points;
+      highscore.innerHTML= "Highscore: "+maxpoints;
+      localStorage.setItem("Score",maxpoints)
+   }
+   if (localStorage.getItem("Score")) {
+      highscore.innerHTML= "Highscore: "+ localStorage.getItem("Score");
 
+   }
+   
+
+
+
+}
  function addSnake() { 
-
- if ( direct==="DOWN") {
-       snake.push({x:snake[snake.length-1]["x"],y:snake[snake.length-1]["y"]-25});
+   points++;
+   score.innerHTML= points;
+  
+      if ( direct==="DOWN") {
+            snake.push({x:snake[snake.length-1]["x"],y:snake[snake.length-1]["y"]-25});
+            food={x:Math.floor(Math.random()*22)*25, y:Math.floor(Math.random()*22)*25};
+      }
+       else if (direct==="UP") {
+          snake.push({x:snake[snake.length-1]["x"],y:snake[snake.length-1]["y"]+25});
+          food={x:Math.floor(Math.random()*22)*25, y:Math.floor(Math.random()*22)*25};
+       }
+       else if (direct==="RIGHT") {
+       snake.push({x:snake[snake.length-1]["x"]-25,y:snake[snake.length-1]["y"]});
        food={x:Math.floor(Math.random()*22)*25, y:Math.floor(Math.random()*22)*25};
- }
-  else if (direct==="UP") {
-     snake.push({x:snake[snake.length-1]["x"],y:snake[snake.length-1]["y"]+25});
-     food={x:Math.floor(Math.random()*22)*25, y:Math.floor(Math.random()*22)*25};
-  }
-  else if (direct==="RIGHT") {
-  snake.push({x:snake[snake.length-1]["x"]-25,y:snake[snake.length-1]["y"]});
-  food={x:Math.floor(Math.random()*22)*25, y:Math.floor(Math.random()*22)*25};
- 
- }
- else if (direct==="LEFT") {
-  snake.push({x:snake[snake.length-1]["x"]+25,y:snake[snake.length-1]["y"]}); 
-  food={x:Math.floor(Math.random()*22)*25, y:Math.floor(Math.random()*22)*25};
- }
+      
+      }
+      else if (direct==="LEFT") {
+       snake.push({x:snake[snake.length-1]["x"]+25,y:snake[snake.length-1]["y"]}); 
+       food={x:Math.floor(Math.random()*22)*25, y:Math.floor(Math.random()*22)*25};
+      }
  }
